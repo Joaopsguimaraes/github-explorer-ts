3faacaa3d91d0e802fd23d8d323166f78767c7fc
Joao Guimaraes <psjoaovictor@gmail.com>
2022-05-05 23:16:04 -0300

    example useEffect and api

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import RepositoryList from "./components/RepositoryList";
+import "../style/global.scss";
+
+const App = () => {
+  return (
+    <>
+      <RepositoryList /> 
+    </>
+  );
+};
+
+export default App;
